2609ac1517f95254e3aae1ef78d42b663a8cfe7a
prosanna <prosanna2005@gmail.com>
2021-07-02 02:52:11 +0530

    Initial commit.

---
+++ movie-catalog-service/src/test/java/io/microServiceWorkShop/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.microServiceWorkShop.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
